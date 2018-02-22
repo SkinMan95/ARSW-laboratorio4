44487f2e311c479612cc2df008253c3e797e24ee
Alejandro Anzola <anzola95avila@gmail.com>
2018-02-22 11:28:59 -0500

    parte 2.2

---
+++ OrdersController.js
@@ -0,0 +1,19 @@
+var list = [
+    {
+	order_id: 1,
+	table_id: 1,
+	products: [
+	    {
+		product: "PIZZA",
+		quantity: 3,
+		price: "$15.000"
+	    },
+	    {
+		product: "HAMBURGER",
+		quantity: 1,
+		price: "$12.300"
+	    }
+	]
+    }
+]
+
